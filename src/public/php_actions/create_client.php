e58d28494446104bd3c813eefa6ae682e0ab08de
Adriel da Silva <silvaadriel510@gmail.com>
2019-09-08 13:13:28 -0300

    clear_input function created

--- src/public/php_actions/create_client.php
+++ src/public/php_actions/create_client.php
@@ -2,10 +2,17 @@
 session_start();
 require_once 'db_connection.php';
 
-$name = pg_escape_string($_POST['name']);
-$last_name = pg_escape_string($_POST['last_name']);
-$email = pg_escape_string($_POST['email']);
-$birth_date = pg_escape_string($_POST['birth_date']);
+function clear_input($input) {
+  $clean_input = pg_escape_string($input);
+  $clean_input = htmlspecialchars($clean_input);
+
+  return $clean_input;
+}
+
+$name = clear_input($_POST['name']);
+$last_name = clear_input($_POST['last_name']);
+$email = clear_input($_POST['email']);
+$birth_date = clear_input($_POST['birth_date']);
 
 $sql = "INSERT INTO clients (name, last_name, email, birth_date) VALUES ('$name', '$last_name', '$email', '$birth_date')";
 $result = pg_query($connect, $sql);
